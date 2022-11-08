ccfa015ac9845ec3516f5220939731533e8ad942
DOMAIN\jose.martinez1 <jmartinezn@intergrupo.com>
2022-11-07 20:44:10 -0500

    proyecto con lista de sitios en clase modelo

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: sort_child_properties_last, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:ptoyecto_ciclo4/Pages/principal.dart';
+import 'package:ptoyecto_ciclo4/Pages/list.dart';
 import 'package:ptoyecto_ciclo4/Pages/registrar_page.dart';
+import 'package:ptoyecto_ciclo4/repository/firebase_api.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,41 +13,51 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final email = TextEditingController();
-  final password = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
 
-  void validarUsuario() {
-    if (email.text.isNotEmpty && password.text.isNotEmpty) {
-      if (email.text == "nomadacolombia@gmail.com") {
-        if (password.text == "nomada1234") {
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => const Principal()));
-        } else {
-          mostrarMensaje("Contraseña Incorrecta");
-        }
-      } else {
-        mostrarMensaje('Usuario no registrado');
-      }
+  //User userload = User.Empty();
+
+  final FirebaseApi _firebaseApi = FirebaseApi();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _validarUsuario() async {
+    if (_email.text.isEmpty && _password.text.isEmpty) {
+      _showMsg('Debe digitar el correo y la contraseña');
     } else {
-      mostrarMensaje('Datos Obligatorios');
+      var result = await _firebaseApi.logInUser(_email.text, _password.text);
+      String msg = '';
+      if (result == 'user-not-found') {
+        msg = 'Correo electrónico no registrado';
+      } else if (result == 'wrong-password') {
+        msg = 'Contraseña incorrecta';
+      } else if (result == 'network-request-failed') {
+        msg = 'Revise su conexión a internet';
+      } else {
+        msg = 'Bienvenido';
+      }
+      _showMsg(msg);
+      if (msg == 'Bienvenido') {
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Lista()));
+      }
     }
   }
 
-  void mostrarMensaje(String mensaje) {
-    final pantalla = ScaffoldMessenger.of(context);
-    pantalla.showSnackBar(SnackBar(
-      content: Text(
-        mensaje,
-        style: TextStyle(fontSize: 18),
+  void _showMsg(String msg) {
+    final Scaffold = ScaffoldMessenger.of(context);
+    Scaffold.showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        action: SnackBarAction(
+            label: 'Aceptar', onPressed: Scaffold.hideCurrentSnackBar),
       ),
-      backgroundColor: Color.fromARGB(255, 190, 76, 68),
-      duration: Duration(seconds: 5),
-      action: SnackBarAction(
-        label: 'Aceptar',
-        textColor: Colors.white,
-        onPressed: pantalla.hideCurrentSnackBar,
-      ),
-    ));
+    );
   }
 
   @override
@@ -72,7 +83,7 @@ class _LoginPageState extends State<LoginPage> {
                   margin: const EdgeInsets.all(30),
                 ),
                 TextFormField(
-                  controller: email,
+                  controller: _email,
                   decoration: InputDecoration(
                       hintText: "Correo elctronico",
                       border: OutlineInputBorder(
@@ -86,7 +97,7 @@ class _LoginPageState extends State<LoginPage> {
                   height: 20,
                 ),
                 TextFormField(
-                  controller: password,
+                  controller: _password,
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: "Contraseña",
@@ -123,7 +134,7 @@ class _LoginPageState extends State<LoginPage> {
                         textStyle: TextStyle(
                             fontStyle: FontStyle.italic, fontSize: 20)),
                     onPressed: () {
-                      validarUsuario();
+                      _validarUsuario();
                     },
                     child: const Text("Ingresar")),
               ],
--- lib/Pages/registrar_page.dart
+++ lib/Pages/registrar_page.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:ptoyecto_ciclo4/Pages/login_page.dart';
+import 'package:ptoyecto_ciclo4/models/user.dart';
+import 'package:ptoyecto_ciclo4/repository/firebase_api.dart';
 
 class RegistrarPage extends StatefulWidget {
   const RegistrarPage({super.key});
@@ -11,6 +13,68 @@ class RegistrarPage extends StatefulWidget {
 }
 
 class _RegistrarPageState extends State<RegistrarPage> {
+  final FirebaseApi _firebaseApi = FirebaseApi();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final _repPassword = TextEditingController();
+
+  // String _data = "Informacion: ";
+
+  void _showMsg(String msg) {
+    final Scaffold = ScaffoldMessenger.of(context);
+    Scaffold.showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        action: SnackBarAction(
+            label: 'Aceptar', onPressed: Scaffold.hideCurrentSnackBar),
+      ),
+    );
+  }
+
+  void mensajeOk(String msgOk) {
+    final pantalla = ScaffoldMessenger.of(context);
+    pantalla.showSnackBar(SnackBar(
+      content: Text(
+        msgOk,
+        style: const TextStyle(fontSize: 16),
+      ),
+      backgroundColor: const Color(0xFF4CAF50),
+      duration: const Duration(seconds: 5),
+    ));
+  }
+
+  void saveUser(User user) async {
+    var result = await _firebaseApi.registerUser(user.email, user.password);
+    String msg = '';
+    String msgOk = '';
+    if (result == 'invalid-email') {
+      msg = 'El Correo electrónico está mal escrito';
+    } else if (result == 'weak-password') {
+      msg = 'La contraseña debe tener minimo 6 digitos';
+    } else if (result == 'email-already-in-use') {
+      msg = 'Ya existe una cuenta con este correo electrónico';
+    } else if (result == 'network-request-failed') {
+      msg = 'Revise su conexión a internet';
+    } else {
+      msgOk = 'Usuario registrado con exito';
+    }
+    mensajeOk(msgOk);
+  }
+
+  void _onRegisterButtonClicked() {
+    setState(() {
+      if (_password.text == _repPassword.text) {
+        var user = User(_email.text, _password.text);
+        saveUser(user);
+        mensajeOk('Usuario registrado con exito');
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LoginPage()));
+      } else {
+        _showMsg("Las contraseñas deben de ser iguales");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,19 +94,22 @@ class _RegistrarPageState extends State<RegistrarPage> {
                   margin: const EdgeInsets.all(30),
                 ),
                 TextFormField(
+                  controller: _email,
                   decoration: InputDecoration(
-                      hintText: "Correo electronico",
+                      labelText: 'email',
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20)),
                       suffixIcon: Icon(
                         Icons.email,
                         color: Colors.lightBlue,
                       )),
+                  keyboardType: TextInputType.text,
                 ),
                 const SizedBox(
                   height: 20,
                 ),
                 TextFormField(
+                  controller: _password,
                   obscureText: true,
                   decoration: InputDecoration(
                       labelText: "Contraseña",
@@ -52,11 +119,13 @@ class _RegistrarPageState extends State<RegistrarPage> {
                         Icons.vpn_key_sharp,
                         color: Colors.lightBlue,
                       )),
+                  keyboardType: TextInputType.text,
                 ),
                 const SizedBox(
                   height: 20,
                 ),
                 TextFormField(
+                  controller: _repPassword,
                   obscureText: true,
                   decoration: InputDecoration(
                       labelText: "Repetir Contraseña",
@@ -66,6 +135,7 @@ class _RegistrarPageState extends State<RegistrarPage> {
                         Icons.vpn_key_sharp,
                         color: Colors.lightBlue,
                       )),
+                  keyboardType: TextInputType.text,
                 ),
                 const SizedBox(
                   height: 30,
@@ -80,10 +150,11 @@ class _RegistrarPageState extends State<RegistrarPage> {
                         textStyle: TextStyle(
                             fontStyle: FontStyle.italic, fontSize: 20)),
                     onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginPage()));
+                      _onRegisterButtonClicked();
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => const LoginPage()));
                     },
                     child: const Text("Registrar"))
               ],
